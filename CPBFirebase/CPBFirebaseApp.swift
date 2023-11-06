3e4ef7e428a45b4dabe59dea6f553239b1df858c
John Flaherty <jflaherty5106@stu.d214.org>
2023-11-06 10:26:23 -0600

    Initial Commit

---
+++ CPBFirebase/CPBFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPBFirebaseApp.swift
+//  CPBFirebase
+//
+//  Created by Jak Flaherty on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CPBFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
